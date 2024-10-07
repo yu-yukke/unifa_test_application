be013d0bfa003b45f2fd2d92e3bddf83a786b516
y.k <yukke5222@gmail.com>
2024-10-08 03:36:57 +0900

    mod: 認可ページアクセスをコントローラーを介さず直接指定

--- app/controllers/authenticates_controller.rb
+++
@@ -1,7 +0,0 @@
-class AuthenticatesController < ApplicationController
-  def show
-    return redirect_to posts_path if authenticated?
-
-    redirect_to oauth_authenticate_url, allow_other_host: true
-  end
-end
