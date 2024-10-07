eca014c0759d66209be66da387b3b7ab529f4cdb
y.k <yukke5222@gmail.com>
2024-10-07 23:59:43 +0900

    feat: ログインフォームの基本機能とviewを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,30 @@ class SessionsController < ApplicationController
   def new
     @form = Forms::LoginForm.new
   end
+
+  def create
+    @form = Forms::LoginForm.new(session_params)
+
+    render :new and return unless @form.valid?
+
+    if user = @form.authenticate
+      session[:current_user_id] = user.user_id
+
+      #TODO: ログイン成功時のリダイレクト先を後から設定すること
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params
+      .require(:forms_login_form)
+      .permit(
+        :user_id,
+        :password
+      )
+  end
 end
--- app/models/forms/login_form.rb
+++ app/models/forms/login_form.rb
@@ -7,4 +7,13 @@ class Forms::LoginForm
     validates :user_id
     validates :password
   end
+
+  # ユーザーIDとパスワードを検証し、一致した場合にユーザー情報を返却する
+  def authenticate
+    user = UserAccount.find_by(user_id: user_id)
+    return user if user && user.authenticate(password)
+
+    errors.add(:base, 'ユーザーIDとパスワードが一致するユーザーが存在しません')
+    false
+  end
 end
