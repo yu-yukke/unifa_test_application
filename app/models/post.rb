cbf9f9a8c0396dbb6cc0c0580ea9de6f02a157a9
y.k <yukke5222@gmail.com>
2024-10-08 00:57:31 +0900

    feat: Postテーブルにactive storageをアタッチ

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+
   belongs_to :user_account
 
-  validates :title, presence: true
+  with_options presence: true do
+    validates :title
+    validates :image,
+              attached: true,
+              content_type: %w[image/png image/jpeg]
+  end
 end
