015b499278c6b8464a69496e992ac6d862ac6618
y.k <yukke5222@gmail.com>
2024-10-08 00:00:32 +0900

    chore: バリデーションメッセージの日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module UnifaTestApplication
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 日本語化対応
+    config.i18n.default_locale = :ja
   end
 end
